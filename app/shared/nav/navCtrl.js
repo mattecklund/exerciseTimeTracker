8c50cf9316ecbc50d7f6edeea907bf375167c5de
Matthew Ecklund <mecklund@ldschurch.org>
2015-06-17 19:18:18 -0600

    added a try/catch so that a lack of user's uid doesn't break the login page

--- app/shared/nav/navCtrl.js
+++ app/shared/nav/navCtrl.js
@@ -1,6 +1,6 @@
 var app = angular.module('exerciseTimeTracker');
 
-app.controller('navCtrl', function($scope, $timeout, $mdSidenav, $mdComponentRegistry, $log){
+app.controller('navCtrl', function($scope, $timeout, $mdSidenav, $mdComponentRegistry, $log, authnService, dashboardService){
 	$scope.links = ['timeTracker', 'dashboard', 'authn', 'terms', 'help', 'contact', 'about'];
 
 	$scope.close = function() {
@@ -16,5 +16,16 @@ app.controller('navCtrl', function($scope, $timeout, $mdSidenav, $mdComponentReg
 	              // $log.debug("toggle left is done");
 	        });
   };
+
+  try {
+    $scope.userUid = authnService.authnObj.$getAuth().uid.replace('simplelogin:', '')
+    $scope.userProfile = dashboardService.getProfile($scope.userUid);
+  } catch(e) {
+    console.log('not logged in');
+    $scope.userProfile = {
+      name: 'Navigation'
+    }
+  }
+
 
 })
